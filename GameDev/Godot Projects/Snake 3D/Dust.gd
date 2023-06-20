4fdb8390e797aac7fb9abe1f4f60bfe63ab7e5a6
Dav Edi <davedi.isme@gmail.com>
2023-06-20 08:28:33 +0300

    add snake 3D

---
+++ GameDev/Godot Projects/Snake 3D/Dust.gd
@@ -0,0 +1,27 @@
+extends Spatial
+
+const NUM_PARTICLES = 50
+const MAX_CLOUD_OFFSET = 16
+
+func _ready():
+	# Spawn a cloud of dust particles
+	for n in NUM_PARTICLES:
+		$Cloud.add_child($Cloud/Box.duplicate())
+	# Randomly position them
+	for box in $Cloud.get_children():
+		box.set_translation(Vector3(get_rand_pos(), get_rand_pos(), get_rand_pos()))
+		box.rotate(Vector3(randf(), randf(), randf()).normalized(), PI)
+
+
+func get_rand_pos():
+	return rand_range(-MAX_CLOUD_OFFSET, MAX_CLOUD_OFFSET)
+
+
+func check_extents(base_pos: Vector3):
+	for box in $Cloud.get_children():
+		var pos = box.translation - base_pos
+		box.translation = base_pos + Vector3(wrap_coor(pos.x), wrap_coor(pos.y), wrap_coor(pos.z))
+
+
+func wrap_coor(n):
+	return wrapf(n, -MAX_CLOUD_OFFSET, MAX_CLOUD_OFFSET)
---
+++ GameDev/Godot Projects/Snake 3D/Snake.gd
@@ -0,0 +1,131 @@
+extends Spatial
+
+signal hit_tail
+
+var tail_piece_scene = preload("res://TailPiece.tscn")
+var displacement = transform.basis.z
+var direction = FORWARD
+
+enum { UP, DOWN, LEFT, RIGHT, FORWARD, BACK }
+
+# In the following functions we change direction according to user input
+# and current direction of movement of the snake head
+func up():
+	match direction:
+		UP:
+			direction = FORWARD
+		DOWN:
+			direction = BACK
+		LEFT:
+			direction = UP
+		RIGHT:
+			direction = UP
+		FORWARD:
+			direction = UP
+		BACK:
+			direction = UP
+
+
+func down():
+	match direction:
+		UP:
+			direction = BACK
+		DOWN:
+			direction = FORWARD
+		LEFT:
+			direction = DOWN
+		RIGHT:
+			direction = DOWN
+		FORWARD:
+			direction = DOWN
+		BACK:
+			direction = DOWN
+
+
+func left():
+	match direction:
+		UP:
+			direction = FORWARD
+		DOWN:
+			direction = FORWARD
+		LEFT:
+			direction = FORWARD
+		RIGHT:
+			direction = BACK
+		FORWARD:
+			direction = LEFT
+		BACK:
+			direction = LEFT
+
+
+func right():
+	match direction:
+		UP:
+			direction = BACK
+		DOWN:
+			direction = BACK
+		LEFT:
+			direction = BACK
+		RIGHT:
+			direction = FORWARD
+		FORWARD:
+			direction = RIGHT
+		BACK:
+			direction = RIGHT
+
+
+func set_displacement():
+	match direction:
+		UP:
+			displacement = transform.basis.y
+		DOWN:
+			displacement = -transform.basis.y
+		LEFT:
+			displacement = -transform.basis.x
+		RIGHT:
+			displacement = transform.basis.x
+		FORWARD:
+			displacement = transform.basis.z
+		BACK:
+			displacement = -transform.basis.z
+
+
+# The way to move is to move the head and set the last tail node to the last position of the head
+# Then position the the tail node as the first child node of the tail.
+# Now the last tail node becomes the end of the tail.
+# Also, we can use the last end_pos as a location to add another tail segment knowing that the location is clear. Also, there can be a number of segments to add rather than just one (as a bool).
+func move_ahead(step: int, tail_segments_to_add: int) -> int:
+	var old_head_pos = $Head.translation
+	var end_pos = $Head.translation
+	$Head.translate(displacement * step)
+	var n = $Tail.get_child_count()
+	if n > 0:
+		var tail_piece = $Tail.get_child(n-1)
+		end_pos = tail_piece.translation
+		$Tail.move_child(tail_piece, 0)
+		tail_piece.translation = old_head_pos
+	
+	if tail_segments_to_add > 0:
+		tail_segments_to_add -= 1
+		var tpi = tail_piece_scene.instance()
+		tpi.connect("area_entered", self, "hit_tail")
+		tpi.translation = end_pos
+		$Tail.add_child(tpi)
+	
+	return tail_segments_to_add
+
+
+func hit_tail(_area):
+	emit_signal("hit_tail")
+
+
+func get_positions():
+	var positions = []
+	for item in $Tail.get_children():
+		positions.append(item.translation)
+	return positions
+
+
+func remove_tail():
+	for tail_segment in $Tail.get_children():
+		tail_segment.queue_free()
